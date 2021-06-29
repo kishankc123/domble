5ef9f751b6dd029d9c5e4e0ee490a621bcb1c1da
Abhiyan Sainju <63235617+aabhiyann@users.noreply.github.com>
2021-06-29 23:43:38 +0545

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import Navbar from './Components/Navbar';
-import {BrowserRouter as Router} from 'react-router-dom'
-
+import "./App.css";
+import Header from "./components/Header";
+import Welcome from "./components/Welcome";
+import Body from "./components/Body";
+import Info from "./components/Info";
 
 function App() {
   return (
-    <Router >
-     <Navbar/>
-    </Router>
+    <div className="App">
+      <Header />
+      <Welcome />
+      <Body />
+      <Info />
+    </div>
   );
 }
 
