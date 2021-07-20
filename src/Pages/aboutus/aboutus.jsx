cd5b56b7b823be4c4f985e39ff9a471a39bf2ee3
kishankc123 <kishancy27@gmail.com>
2021-07-20 12:15:10 +0545

    commmit

--- src/Pages/aboutus/aboutus.jsx
+++ src/Pages/aboutus/aboutus.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from "react"; 
 import FormHeader from "../detailform/header_detailform";
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 8e26d9567143830af7bff6e48af4873b6f093dc5
 class AboutUs extends Component{
 
     render(){
